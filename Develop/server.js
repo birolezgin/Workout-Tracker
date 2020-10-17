b777e6ca5edb6971833c7708cd6c00f5e086ada5
birolezgin <b.ezginksf@gmail.com>
2020-10-16 22:36:08 -0700

    Server.js generated

---
+++ Develop/server.js
@@ -0,0 +1,33 @@
+// Server.js - This file is the initial starting point for the Node/Express server.
+
+// *** Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express App
+const app = express();
+
+app.use(logger("dev"));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
