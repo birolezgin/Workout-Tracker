6b183ea71288e2533376eccd0e22fdcb501deaf1
birolezgin <b.ezginksf@gmail.com>
2020-10-17 19:17:09 -0700

    api-routes.js generated

---
+++ Develop/routes/api-routes.js
@@ -0,0 +1,58 @@
+const Workout = require("../models/workoutModel");
+
+module.exports = function (app) {
+
+    app.get("/api/workouts", function (req, res) {
+      Workout.find()
+      .then(data => {
+          res.json(data)
+      })
+      .catch(err => {
+          res.json(err)
+      })
+    });
+
+    app.post("/api/workouts", function (req, res) {
+      Workout.create({})
+        .then(data => res.json(data))
+        .catch(err => {
+            console.log("err", err)
+            res.json(err)
+        })
+    });
+
+    app.put("/api/workouts/:id", ({ body, params }, res) => {
+        Workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercise: body }},
+            { new: true, runValidators: true }
+        )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.get("/api/workouts/range", (req, res) => {
+      Workout.find({}).limit(7)
+      .then(dbWorkouts => {
+          console.log("dbWorkouts");
+          res.json(dbWorkouts);
+      })
+      .catch(err => {
+          res.json(err);
+      });
+    });
+
+    app.delete("/api/workouts", ({ body }, res) => {
+        Workout.findByIdAndDelete(body.id)
+          .then(() => {
+              res.json(true);
+          })
+          .catch(err => {
+            res.json(err);
+          });
+    });
+}
