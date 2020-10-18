52bc186fc054d2de489464c7cda00520da419426
birolezgin <b.ezginksf@gmail.com>
2020-10-17 21:35:59 -0700

    PORT no changed

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express App
 const app = express();
