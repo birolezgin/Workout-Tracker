97c7e30538262eceaaa01c62a81bbdbcebf2fa51
birolezgin <b.ezginksf@gmail.com>
2020-10-18 20:16:33 -0700

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -21,6 +21,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
