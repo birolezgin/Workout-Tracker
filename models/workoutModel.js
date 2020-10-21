0036334a0ceb78b47a17223e4e0425d7785b82af
birolezgin <b.ezginksf@gmail.com>
2020-10-20 17:00:40 -0700

    workoutModel.js modified

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -2,42 +2,44 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  day: {
-      type: Date,
-      default: () => new Date()
-  },
-  exercices: [{
-      type: {
-          type: String,
-          trim: true,
-          required: "Enter an exercise type"
-      },
-      name: {
-          type: String,
-          trim: true,
-          required: "Enter an exercise name"
-      },
-      duration: {
-          type: Number,
-          required: "Enter duration of workout in minutes"
-      },
-      weight: {
-          type: Number
-      },
-      reps: {
-          type: Number
-      },
-      sets: {
-          type: Number
-      },
-      distance: {
-          type: Number
-      }
-    }]  
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercises: [
+            {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter an exercise type"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter an exercise name"
+            },
+            duration: {
+                type: Number,
+                required: "Enter duration of workout in minutes"
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
 
+        ]
     },
-
     {
         toJSON: {
             // includes virtual properties when data is presented
